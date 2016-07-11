81e78deac3058b0dded47f4157d0361e77de8ff3
kirill <apuc06@mail.ru>
2016-07-11 15:44:58 +0300

    test video

--- views/header.php
+++ views/header.php
@@ -24,27 +24,5 @@
     <!-- elFinder translation (OPTIONAL) -->
     <!--<script src="js/i18n/elfinder.ru.js"></script>-->
 
-    <!-- elFinder initialization (REQUIRED) -->
-    <script type="text/javascript" charset="utf-8">
-        // Documentation for client options:
-        // https://github.com/Studio-42/elFinder/wiki/Client-configuration-options
-
-        // Helper function to get parameters from the query string.
-        function getUrlParam(paramName) {
-            var reParam = new RegExp('(?:[\?&]|&amp;)' + paramName + '=([^&]+)', 'i') ;
-            var match = window.location.search.match(reParam) ;
-
-            return (match && match.length > 1) ? match[1] : '' ;
-        }
-        var h = document.documentElement.clientHeight;
-        $().ready(function() {
-            var elf = $('#elfinder').elfinder({
-                lang: 'ru',
-                url : 'elfinder/php/connector.php',
-                resizable: false,
-                height: h - 63
-            }).elfinder('instance');
-        });
-    </script>
 </head>
 <body>
